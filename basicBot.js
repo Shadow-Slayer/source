f6d0051525fc8bf3352e6195e1a477ef8b27271d
Shadow-Slayer <wander.p.c.07@gmail.com>
2016-06-19 17:56:56 -0300

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -310,7 +310,7 @@
             fbLink: "http://www.fb.com/groups/MoveItBrasil ",
             youtubeLink: null,
             website: "http://goo.gl/O4tlVo",
-            intervalMessages: ["!roleta",":large_orange_diamond: Roleta a cada 6 músicas, fique atento ;) :large_orange_diamond:",":large_orange_diamond: Participe do nosso Grupo no Facebook http://www.fb.com/groups/MoveItBrasil <3 :large_orange_diamond:"],
+            intervalMessages: ["!roleta",":large_orange_diamond: Curta nossa página no Facebook http://www.facebook.com/moveitbrasil :large_orange_diamond: ",":large_orange_diamond: Roleta a cada 6 músicas, fique atento ;) :large_orange_diamond:",":large_orange_diamond: Participe do nosso Grupo no Facebook http://www.fb.com/groups/MoveItBrasil <3 :large_orange_diamond:"],
             messageInterval: 2,
             songstats: false,
             commandLiteral: "!",
